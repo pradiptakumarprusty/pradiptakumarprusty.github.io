086c3189c2e13584f1bf1efceb09b437bcd7e509
pradiptakumarprusty <pradiptakumarprusty2006@gmail.com>
2026-09-06 15:05:29 +0530

    Update header offset for scroll positioning

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (targetSection) {
                     e.preventDefault();
 
-                    const headerOffset = 110;
+                    const headerOffset = 170;
 
 const elementPosition = targetSection.getBoundingClientRect().top;
 const offsetPosition = elementPosition + window.scrollY - headerOffset;
